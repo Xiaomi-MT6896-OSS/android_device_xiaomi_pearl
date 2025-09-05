78bdef7d76844dcf28975e6042771c68aefc745c
Enceka <enceka@yeah.net>
2025-09-05 16:46:35 +0800

    pearl: Use android.hardware.thermal-service.mediatek and no longer extract wifi config

--- device.mk
+++ device.mk
@@ -357,5 +357,12 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/mediatek/libmtkperf_client
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.mediatek
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/pearl/pearl-vendor.mk)
