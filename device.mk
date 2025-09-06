5d7ef03581c9c8a9f7702dfe738e44c18da96abc
Enceka <enceka@yeah.net>
2025-09-06 20:12:40 +0800

    pearl: Use composer 2.3

--- device.mk
+++ device.mk
@@ -102,7 +102,7 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.4-service \
+    android.hardware.graphics.composer@2.3-service \
     android.hardware.memtrack-service.mediatek-mali
 
 # DRM
