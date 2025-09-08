263a3fc2d97c937cd3a9989098343bc6719c0315
Enceka <enceka@yeah.net>
2025-09-08 16:15:01 +0800

    pearl: blob: Some blob seemed to be fixup in 207

--- extract-files.py
+++ extract-files.py
@@ -52,9 +52,7 @@ blob_fixups: blob_fixups_user_type = {
      'vendor/lib64/mt6895/libmtkcam_request_requlator.so'): blob_fixup()
         .add_needed('libprocessgroup_shim.so'),
 
-    ('vendor/lib64/lib3a.ae.pipe.so',
-     'vendor/lib64/mt6895/libaaa_toneutil.so',
-     'vendor/lib64/mt6895/lib3a.flash.so',
+    ('vendor/lib64/mt6895/lib3a.flash.so',
      'vendor/lib64/mt6895/lib3a.sensors.color.so',
      'vendor/lib64/mt6895/lib3a.sensors.flicker.so'): blob_fixup()
         .add_needed('liblog.so'),
@@ -78,11 +76,6 @@ blob_fixups: blob_fixups_user_type = {
      'vendor/lib64/libsysenv.so',
      'vendor/bin/ioprofiler'): blob_fixup()
         .add_needed('libbase_shim.so'),
-
-     'vendor/lib64/libthha.so': blob_fixup()
-        .clear_symbol_version('__aeabi_memcpy')
-        .clear_symbol_version('__aeabi_memset')
-        .clear_symbol_version('__gnu_Unwind_Find_exidx'),
 
      'vendor/lib64/mt6895/libneuralnetworks_sl_driver_mtk_prebuilt.so': blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
