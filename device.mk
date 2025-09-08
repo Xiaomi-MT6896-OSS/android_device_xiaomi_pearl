a21dd36344c055176f23b937c43c37d6e68c5b0a
Enceka <enceka@yeah.net>
2025-09-09 01:35:08 +0800

    pearl: Do not manually build dependencies

--- device.mk
+++ device.mk
@@ -65,14 +65,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.soundtrigger@2.3-impl \
-    android.hardware.audio.service
+    android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
     audio.primary.default \
     audio.bluetooth.default \
-    audio.usb.default \
-    android.hardware.bluetooth.audio-impl
+    audio.usb.default
 
 PRODUCT_PACKAGES += \
     MtkInCallService
